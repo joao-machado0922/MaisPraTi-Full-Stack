f42d0f1bc4bfd4d040ea5a6884dad1c9f7fb1cde
João Pedro <joaopedrocode0@gmail.com>
2026-07-07 15:54:26 -0300

    Aula 28 - Tela de Login
    Criada tela de login
    Criados métodos de cadastro e login
    Criado autenticador

--- modulo2/site-news/js/pages/cadastro.js
+++ modulo2/site-news/js/pages/cadastro.js
@@ -1,7 +1,33 @@
 import { buscarCep } from "../services/viacep.js";
+import { registrar } from "../services/autenticador.js";
 
 let form = document.querySelector('#form-cadastro');
 let campoCep = document.querySelector('#cep');
+let aviso = document.querySelector('#aviso');
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const usuario = {
+        nome: document.querySelector("#nome").value,
+        email: document.querySelector("#email").value,
+        senha: document.querySelector("#senha").value,
+        cep: campoCep.value,
+        logradouro: document.querySelector('#logradouro').value,
+        bairro: document.querySelector('#bairro').value,
+        cidade: document.querySelector('#cidade').value,
+        uf: document.querySelector('#uf').value
+    }
+
+    try {
+        registrar(usuario);
+        alert("Cadastro realizado! Faça login para continuar.");
+        aviso.textContent = "";
+        window.location.href = "login.html";
+    } catch (erro) {
+        aviso.textContent = erro.message;
+    }
+})
 
 campoCep.addEventListener('blur', async () => {
     if (!campoCep.value) return
